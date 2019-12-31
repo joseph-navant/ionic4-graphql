9258124dd8bdd0e91754fbd17c98cceab49878d4
Joseph <joseph.morantnavarro@gmail.com>
2019-12-31 13:13:03 +0100

    Unsubscribing from user list

--- frontend/src/app/pages/user-list/user-list.page.ts
+++ frontend/src/app/pages/user-list/user-list.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user';
 
@@ -8,11 +9,18 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./user-list.page.scss']
 })
 export class UserListPage {
+  usersSubscription: Subscription;
   users: User[];
 
   constructor(private userService: UserService) {}
 
   ionViewWillEnter() {
-    this.userService.getUserList().subscribe(users => (this.users = users));
+    this.usersSubscription = this.userService
+      .getUserList()
+      .subscribe(users => (this.users = users));
+  }
+
+  ionViewWillLeave() {
+    this.usersSubscription.unsubscribe();
   }
 }
